795aacdaf5742ace8c30ff519af43a0798684dc9
Aaron Kling <webgeek1234@gmail.com>
2019-02-27 10:29:35 -0600

    Add nvgpu hals, manifest, and permissions
    Nvgpu supports aep, vulkan 1.0.3, and opengl 4.5
    
    Change-Id: I37a5107988f40a7e9b6a054c348fa1a6e1d2bfda

--- t210.mk
+++ t210.mk
@@ -16,6 +16,7 @@
 
 TARGET_TEGRA_VERSION  := t210
 TARGET_TEGRA_KEYSTORE ?= nvkeystore
+TARGET_TEGRA_GPU      ?= nvgpu
 
 # System properties
 include $(LOCAL_PATH)/system_prop.mk
@@ -25,5 +26,15 @@ PRODUCT_PACKAGES += \
     init.t210_common.rc \
     init.tlk.rc \
     ueventd.t210ref.rc
+
+# NVGPU permissions
+ifeq ($(TARGET_TEGRA_GPU),nvgpu)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.level-1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
+    frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml \
+    $(LOCAL_PATH)/permissions/com.nvidia.feature.opengl4.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nvidia.feature.opengl4.xml \
+    $(LOCAL_PATH)/permissions/com.nvidia.nvsi.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.nvidia.nvsi.xml
+endif
 
 $(call inherit-product, device/nvidia/tegra-common/tegra.mk)
