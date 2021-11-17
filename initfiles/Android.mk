78b1ded4b257e880ffa3e8cb1039ed5a45a5040a
Aaron Kling <webgeek1234@gmail.com>
2021-11-16 23:19:46 -0600

    Move ueventd conf to vendor etc folder
    system/core removed support for having the conf in /vendor/ in
    commit 71dd706.
    
    Change-Id: I4a3648d168c9f8cdf43f90cdf244f4d2dcda1e87

--- initfiles/Android.mk
+++ initfiles/Android.mk
@@ -17,9 +17,9 @@ LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.t210ref.rc
-LOCAL_MODULE_STEM  := ueventd.rc
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := ueventd.t210ref.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+LOCAL_MODULE        := ueventd.t210ref.rc
+LOCAL_MODULE_STEM   := ueventd.rc
+LOCAL_MODULE_CLASS  := ETC
+LOCAL_SRC_FILES     := ueventd.t210ref.rc
+LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
