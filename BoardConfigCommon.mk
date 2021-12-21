da6fcff0ca38215d770a3e6a3bcdd22813f8a255
Aaron Kling <webgeek1234@gmail.com>
2021-12-21 11:42:26 -0600

    Support building 32-bit targets
    Change-Id: I7b0fefc804888c8acd198c4feaf33e00a6a4167d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -15,6 +15,7 @@
 #
 
 # Architecture
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_SMP := true
@@ -27,6 +28,16 @@ TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_VARIANT := cortex-a53.a57
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
+else
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a53.a57
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_KERNEL_ARCH := arm64
+TARGET_USES_64_BIT_BINDER := true
+endif
 
 # Board
 TARGET_BOARD_PLATFORM := tegra
