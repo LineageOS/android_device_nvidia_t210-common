aa0a0ec9bf5f535def4af40877a0da00bf044bd9
Aaron Kling <webgeek1234@gmail.com>
2023-06-01 10:50:21 -0500

    Add linux-firmware vendor alternative
    For use with the mainline kernel and libdrm
    
    Change-Id: I96d8f81bac6823d5623c1d6145a0a15b24a9e690

---
+++ vendor/linux-firmware/t210-recovery.mk
@@ -0,0 +1,24 @@
+# Copyright (C) 2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+T210_FIRMWARE_PATH   := kernel/nvidia/linux-firmware
+DOWNSTREAM_BRANCH    := rel-shield-r
+T210_DOWNSTREAM_PATH := vendor/nvidia/t210/$(DOWNSTREAM_BRANCH)/firmware
+
+# Firmware
+PRODUCT_COPY_FILES += \
+    $(T210_FIRMWARE_PATH)/nvidia/tegra210/xusb.bin:recovery/root/lib/firmware/nvidia/tegra210/xusb.bin \
+    $(T210_DOWNSTREAM_PATH)/xusb/tegra210b01_xusb_firmware:recovery/root/lib/firmware/nvidia/tegra210b01/xusb.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/tegra210/xusb.bin:$(TARGET_COPY_OUT_RAMDISK)/lib/firmware/nvidia/tegra210/xusb.bin \
+    $(T210_DOWNSTREAM_PATH)/xusb/tegra210b01_xusb_firmware:$(TARGET_COPY_OUT_RAMDISK)/lib/firmware/nvidia/tegra210b01/xusb.bin
