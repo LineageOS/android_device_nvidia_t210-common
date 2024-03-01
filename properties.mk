77c493c3796f3dad0d4baa006a96cf856e943cea
Aaron Kling <webgeek1234@gmail.com>
2024-02-29 18:27:34 -0600

    initfiles: Remove some unsued properties
    Change-Id: Ideb372249cbba37477b3516cfdf6a86ed360f982

--- properties.mk
+++ properties.mk
@@ -1,12 +1,7 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
     af.resampler.quality=7 \
-    audio.deep_buffer.media=1 \
-    af.fast_downmix=1
+    audio.deep_buffer.media=1
 
 PRODUCT_SYSTEM_PROPERTY_OVERRIDES += \
     ro.af.client_heap_size_kbyte=10240
-
-# Input
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.input.noresample=1
