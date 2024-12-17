2c1b8476a22419deadd249292ea977a5cd81510f
Aaron Kling <webgeek1234@gmail.com>
2024-12-16 23:38:55 -0600

    vendor: Use copy-files for firmware targets
    There's no real build time checks that the build system can do for these
    type of files. Plus duplicate copy-files entries are now a fatal error,
    so there's no benefit to keeping these as modules.
    
    Change-Id: I1bf7cfb502617b37eb6cd71d029ab4e4ba66ec6f

--- vendor/linux-firmware/t210.mk
+++ vendor/linux-firmware/t210.mk
@@ -12,33 +12,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := device/nvidia/t210-common/vendor/linux-firmware
+T210_FIRMWARE_PATH := kernel/nvidia/linux-firmware
 
-include $(LOCAL_PATH)/t210-recovery.mk
+DOWNSTREAM_BRANCH    := rel-shield-r
+T210_DOWNSTREAM_PATH := vendor/nvidia/t210/$(DOWNSTREAM_BRANCH)/firmware
 
 # GPU firmware
-PRODUCT_PACKAGES += \
-    bl.bin \
-    ucode_load.bin \
-    fecs_bl.bin \
-    fecs_data.bin \
-    fecs_inst.bin \
-    fecs_sig.bin \
-    gpccs_data.bin \
-    gpccs_inst.bin \
-    sw_bundle_init.bin \
-    sw_method_init.bin \
-    sw_ctx.bin \
-    sw_nonctx.bin \
-    desc.bin \
-    image.bin \
-    sig.bin
+PRODUCT_COPY_FILES += \
+    $(T210_FIRMWARE_PATH)/nvidia/gm20b/acr/bl.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/acr/bl.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/gm20b/acr/ucode_load.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/acr/ucode_load.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/fecs_bl.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/fecs_bl.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/fecs_data.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/fecs_data.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/fecs_inst.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/fecs_inst.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/fecs_sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/fecs_sig.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/gpccs_data.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/gpccs_data.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/gpccs_data.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/gpccs_data.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/sw_bundle_init.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/sw_bundle_init.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/gm200/gr/sw_method_init.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/sw_method_init.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/sw_ctx.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/sw_ctx.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/sw_nonctx.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/sw_nonctx.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/gm20b/pmu/desc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/pmu/desc.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/gm20b/pmu/image.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/pmu/image.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/gm20b/pmu/sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/pmu/sig.bin
 
 # General firmware
-PRODUCT_PACKAGES += \
-    nvdec.bin \
-    nvenc.bin \
-    nvjpg.bin \
-    vic04_ucode.bin \
-    xusb.bin \
-    xusb_b01.bin
+PRODUCT_COPY_FILES += \
+    $(T210_DOWNSTREAM_PATH)/tegra21x/nvhost_nvdec020_ns.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra210/nvdec.bin \
+    $(T210_DOWNSTREAM_PATH)/tegra21x/nvhost_nvenc050.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra210/nvenc.bin \
+    $(T210_DOWNSTREAM_PATH)/tegra21x/nvhost_nvjpg010.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra210/nvjpg.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/tegra210/vic04_ucode.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra210/vic04_ucode.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/tegra210/xusb.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra210/xusb.bin \
+    $(T210_DOWNSTREAM_PATH)/xusb/tegra210b01_xusb_firmware:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra210b01/xusb.bin
+
+
+# Recovery / Boot Ramdisk
+PRODUCT_COPY_FILES += \
+    $(T210_FIRMWARE_PATH)/nvidia/tegra210/xusb.bin:recovery/root/lib/firmware/nvidia/tegra210/xusb.bin \
+    $(T210_DOWNSTREAM_PATH)/xusb/tegra210b01_xusb_firmware:recovery/root/lib/firmware/nvidia/tegra210b01/xusb.bin \
+    $(T210_FIRMWARE_PATH)/nvidia/tegra210/xusb.bin:$(TARGET_COPY_OUT_RAMDISK)/lib/firmware/nvidia/tegra210/xusb.bin \
+    $(T210_DOWNSTREAM_PATH)/xusb/tegra210b01_xusb_firmware:$(TARGET_COPY_OUT_RAMDISK)/lib/firmware/nvidia/tegra210b01/xusb.bin
