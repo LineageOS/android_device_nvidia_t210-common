403a480f903e5f7d8743159df46d51a9b33eabb2
Aaron Kling <webgeek1234@gmail.com>
2025-09-07 22:17:46 -0500

    linux-firmware: Use aosp style linux-firmware
    Change-Id: I355c55102edc6c8ec1f5fd12fd51479993d1244d

--- vendor/linux-firmware/t210.mk
+++ vendor/linux-firmware/t210.mk
@@ -12,36 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-T210_FIRMWARE_PATH := kernel/nvidia/linux-firmware
+T210_FIRMWARE_PATH := external/linux-firmware-mainline/firmware
 
 DOWNSTREAM_BRANCH    := rel-shield-r
 T210_DOWNSTREAM_PATH := vendor/nvidia/t210/$(DOWNSTREAM_BRANCH)/firmware
 
-# GPU firmware
-PRODUCT_COPY_FILES += \
-    $(T210_FIRMWARE_PATH)/nvidia/gm20b/acr/bl.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/acr/bl.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/gm20b/acr/ucode_load.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/acr/ucode_load.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/fecs_bl.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/fecs_bl.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/fecs_data.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/fecs_data.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/fecs_inst.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/fecs_inst.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/fecs_sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/fecs_sig.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/gpccs_data.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/gpccs_data.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/gpccs_inst.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/gpccs_inst.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/sw_bundle_init.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/sw_bundle_init.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/gm200/gr/sw_method_init.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/sw_method_init.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/sw_ctx.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/sw_ctx.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/gm20b/gr/sw_nonctx.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/gr/sw_nonctx.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/gm20b/pmu/desc.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/pmu/desc.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/gm20b/pmu/image.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/pmu/image.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/gm20b/pmu/sig.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/gm20b/pmu/sig.bin
+# Upstream firmware
+PRODUCT_PACKAGES += \
+    linux_firmware_nvidia-tegra210
 
 # General firmware
 PRODUCT_COPY_FILES += \
     $(T210_DOWNSTREAM_PATH)/tegra21x/nvhost_nvdec020_ns.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra210/nvdec.bin \
     $(T210_DOWNSTREAM_PATH)/tegra21x/nvhost_nvenc050.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra210/nvenc.bin \
     $(T210_DOWNSTREAM_PATH)/tegra21x/nvhost_nvjpg010.fw:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra210/nvjpg.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/tegra210/vic04_ucode.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra210/vic04_ucode.bin \
-    $(T210_FIRMWARE_PATH)/nvidia/tegra210/xusb.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra210/xusb.bin \
     $(T210_DOWNSTREAM_PATH)/xusb/tegra210b01_xusb_firmware:$(TARGET_COPY_OUT_VENDOR)/firmware/nvidia/tegra210b01/xusb.bin
 
 
